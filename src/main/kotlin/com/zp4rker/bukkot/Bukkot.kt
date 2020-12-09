0d6f1d39b0ecc9c82504ab9df2bb8deb5b8bd5d7
zp4rker <acc@zp4rker.com>
2020-12-10 10:27:24 +1100

    added metrics, marked as non-legacy plugin
    ready for release

--- src/main/kotlin/com/zp4rker/bukkot/Bukkot.kt
+++ src/main/kotlin/com/zp4rker/bukkot/Bukkot.kt
@@ -1,9 +1,17 @@
 package com.zp4rker.bukkot
 
+import org.bstats.bukkit.Metrics
 import org.bukkit.plugin.java.JavaPlugin
 
 /**
  * @author zp4rker
  */
 class Bukkot : JavaPlugin() {
+
+    override fun onEnable() {
+        initMetrics()
+    }
+
+    private fun initMetrics() = Metrics(this, 9605)
+
 }
