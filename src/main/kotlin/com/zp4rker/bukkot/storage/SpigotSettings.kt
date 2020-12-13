ad8fc23ac2c21d87a49f76cc60b0f615045923c6
zp4rker <acc@zp4rker.com>
2020-12-13 23:14:05 +1100

    added spigot settings

---
+++ src/main/kotlin/com/zp4rker/bukkot/storage/SpigotSettings.kt
@@ -0,0 +1,19 @@
+package com.zp4rker.bukkot.storage
+
+import org.bukkit.configuration.file.YamlConfiguration
+import java.io.File
+
+/**
+ * @author zp4rker
+ */
+class SpigotSettings(file: File) : YamlConfiguration() {
+
+    init {
+        load(file)
+    }
+
+    companion object {
+        val INSTANCE: SpigotSettings get() = SpigotSettings(File("spigot.yml"))
+    }
+
+}
