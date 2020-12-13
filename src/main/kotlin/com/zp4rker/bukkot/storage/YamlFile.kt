cdb0cc21c34bb37864e6d4220b02ac812d99bda9
zp4rker <acc@zp4rker.com>
2020-12-13 23:14:16 +1100

    added custom yaml file

---
+++ src/main/kotlin/com/zp4rker/bukkot/storage/YamlFile.kt
@@ -0,0 +1,37 @@
+package com.zp4rker.bukkot.storage
+
+import org.bukkit.configuration.file.YamlConfiguration
+import org.bukkit.plugin.java.JavaPlugin
+import java.io.File
+
+/**
+ * @author zp4rker
+ */
+class YamlFile(plugin: JavaPlugin, name: String): YamlConfiguration() {
+
+    private val file: File
+
+    init {
+        val fileName = if (name.endsWith(".yml")) name else "$name.yml"
+        file = File(plugin.dataFolder, fileName)
+
+        if (!file.parentFile.exists()) file.parentFile.mkdirs()
+        if (file.length() < 1) file.createNewFile()
+        else load(file)
+    }
+
+    fun save() = save(file)
+
+    fun reload() = load(file)
+
+    fun copyDefaults(override: Boolean = false) {
+        if (!file.parentFile.exists()) file.parentFile.mkdirs()
+        if (file.length() < 1) {
+            file.createNewFile()
+            file.writer().use { saveToString() }
+        } else {
+            if (override) file.writer().use { saveToString() }
+        }
+    }
+
+}
