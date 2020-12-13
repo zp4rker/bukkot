8b94b755f7bfb29973c8466e93cbc5893ce18ff7
zp4rker <acc@zp4rker.com>
2020-12-13 23:13:53 +1100

    added some tick timeunit functions/class

---
+++ src/main/kotlin/com/zp4rker/bukkot/extension/TimeUnitExtensions.kt
@@ -0,0 +1,35 @@
+package com.zp4rker.bukkot.extension
+
+import java.util.concurrent.TimeUnit
+
+/**
+ * @author zp4rker
+ */
+
+fun TimeUnit.toTicks(d: Long): Long {
+    return when (this) {
+        TimeUnit.NANOSECONDS -> d / 50000000
+
+        TimeUnit.MICROSECONDS -> d / 50000
+
+        TimeUnit.MILLISECONDS -> d / 50
+
+        TimeUnit.SECONDS -> d * 20
+
+        TimeUnit.MINUTES -> d * 60 * 20
+
+        TimeUnit.HOURS -> d * 60 * 60 * 20
+
+        TimeUnit.DAYS -> d * 24 * 60 * 60 * 20
+    }
+}
+
+object TickTimeUnit {
+    fun toNanos(ticks: Long) = ticks * 50000000
+    fun toMicros(ticks: Long) = ticks * 50000
+    fun toMillis(ticks: Long) = ticks * 50
+    fun toSeconds(ticks: Long) = ticks / 20
+    fun toMinutes(ticks: Long) = ticks / 20 / 60
+    fun toHours(ticks: Long) = ticks / 20 / 60 / 60
+    fun toDays(ticks: Long) = ticks / 20 / 60 / 60 / 24
+}
