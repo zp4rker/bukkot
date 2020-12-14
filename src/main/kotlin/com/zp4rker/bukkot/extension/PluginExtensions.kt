13e7db4f154a68aad0e759010a16a5d59209e460
zp4rker <acc@zp4rker.com>
2020-12-14 11:56:19 +1100

    implemented plugin extensions (scheduler)

---
+++ src/main/kotlin/com/zp4rker/bukkot/extension/PluginExtensions.kt
@@ -0,0 +1,56 @@
+package com.zp4rker.bukkot.extension
+
+import org.bukkit.plugin.Plugin
+import org.bukkit.scheduler.BukkitRunnable
+import org.bukkit.scheduler.BukkitTask
+
+/**
+ * @author zp4rker
+ */
+
+fun Plugin.runTask(delay: Long = 0, async: Boolean = false, task: BukkitRunnable.() -> Unit) : BukkitTask {
+    return if (delay > 0) {
+        if (async) {
+            LambdaRunnable(task).runTaskLaterAsynchronously(this, delay)
+        } else {
+            LambdaRunnable(task).runTaskLater(this, delay)
+        }
+    } else {
+        if (async) {
+            LambdaRunnable(task).runTaskAsynchronously(this)
+        } else {
+            LambdaRunnable(task).runTask(this)
+        }
+    }
+}
+
+fun Plugin.runTaskTimer(delay: Long = 0, period: Long = 20, async: Boolean = false, task: BukkitRunnable.() -> Unit) : BukkitTask {
+    return if (async) {
+        LambdaRunnable(task).runTaskTimerAsynchronously(this, delay, period)
+    } else {
+        LambdaRunnable(task).runTaskTimer(this, delay, period)
+    }
+}
+
+fun Plugin.repeatTask(repeat: IntProgression, delay: Long = 0, period: Long = 20, async: Boolean = false, task: BukkitRunnable.(Int) -> Unit) : BukkitTask {
+    val runnable = object : BukkitRunnable() {
+        val i = repeat.iterator()
+
+        override fun run() {
+            if (i.hasNext()) task(i.nextInt())
+            else cancel()
+        }
+    }
+
+    return if (async) {
+        runnable.runTaskTimerAsynchronously(this, delay, period)
+    } else {
+        runnable.runTaskTimer(this, delay, period)
+    }
+}
+
+internal class LambdaRunnable(private val task: BukkitRunnable.() -> Unit) : BukkitRunnable() {
+    override fun run() {
+        task()
+    }
+}
