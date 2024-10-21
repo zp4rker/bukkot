050ad5c63ca58b5b2b86db0f63667e63c111d623
Zaeem Parker <iam@zp4rker.com>
2024-10-22 10:28:44 +1100

    add `Component` extensions

---
+++ src/main/kotlin/com/zp4rker/bukkot/extensions/Component.kt
@@ -0,0 +1,13 @@
+package com.zp4rker.bukkot.extensions
+
+import net.kyori.adventure.text.Component
+import net.kyori.adventure.text.serializer.json.JSONComponentSerializer
+import net.kyori.adventure.text.serializer.legacy.LegacyComponentSerializer
+import net.kyori.adventure.text.serializer.plain.PlainTextComponentSerializer
+
+/**
+ * @author zp4rker
+ */
+fun Component.legacy() = LegacyComponentSerializer.legacySection().serialize(this)
+fun Component.json() = JSONComponentSerializer.json().serialize(this)
+fun Component.plain() = PlainTextComponentSerializer.plainText().serialize(this)
