85377b88f66e5dd076a830d14c598de2c0eeaea8
Zaeem Parker <iam@zp4rker.com>
2024-10-22 10:29:30 +1100

    add String extension to convert to Component

--- src/main/kotlin/com/zp4rker/bukkot/extensions/String.kt
+++ src/main/kotlin/com/zp4rker/bukkot/extensions/String.kt
@@ -1,10 +1,13 @@
+@file:Suppress("deprecation")
 package com.zp4rker.bukkot.extensions
 
+import net.kyori.adventure.text.Component
 import org.bukkit.ChatColor
 
 /**
  * @author zp4rker
  */
 fun String.colorify(char: Char = '&') = ChatColor.translateAlternateColorCodes(char, this)
-
 fun String.stripColors() = ChatColor.stripColor(this)
+
+fun String.component() = Component.text(this)
