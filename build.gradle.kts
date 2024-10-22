23b3c96ceab8efc0f11bf9d5050860dfe3fb6a58
Zaeem Parker <iam@zp4rker.com>
2024-10-22 14:55:32 +1100

    setup publishing

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,6 +4,9 @@ plugins {
 
     id("com.gradleup.shadow") version "8.3.3"
 
+    `maven-publish`
+    signing
+
     id("xyz.jpenilla.run-paper") version "2.3.1"
 }
 
@@ -21,24 +24,26 @@ repositories {
 }
 
 dependencies {
-    compileOnly(kotlin("stdlib"))
-    compileOnly(kotlin("reflect"))
-    compileOnly("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.9.0")
+    implementation(kotlin("stdlib"))
+    implementation(kotlin("reflect"))
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.9.0")
 
-    compileOnly("io.papermc.paper:paper-api:$mcVersion-R0.1-SNAPSHOT")
+    implementation("io.papermc.paper:paper-api:$mcVersion-R0.1-SNAPSHOT")
 
     implementation("org.bstats:bstats-bukkit:1.7")
 }
 
 tasks {
-    val filename = "${project.name}-${project.version}.jar"
     jar {
-        archiveFileName = "original-$filename"
+        archiveClassifier = "original"
     }
 
     shadowJar {
-        archiveFileName = filename
+        archiveClassifier = ""
         relocate("org.bstats", "com.zp4rker.bukkot.bstats")
+        dependencies {
+            include { it.moduleGroup == "org.bstats" }
+        }
     }
 
     processResources {
@@ -55,10 +60,81 @@ tasks {
         dependsOn(shadowJar)
     }
 
-    runServer {
-        minecraftVersion(mcVersion)
-        subprojects.forEach {
-            pluginJars(it.tasks.jar.map { jar -> jar.archiveFile })
+    create<Jar>("javadocJar") {
+        archiveClassifier = "javadoc"
+        from(javadoc)
+    }
+    create<Jar>("sourcesJar") {
+        archiveClassifier = "sources"
+        from(sourceSets["main"].allSource)
+    }
+}
+
+val sources = artifacts.add("archives", tasks["sourcesJar"])
+val javadocs = artifacts.add("archives", tasks["javadocJar"])
+val final = artifacts.add("archives", tasks["shadowJar"])
+
+publishing {
+    publications {
+        create<MavenPublication>("bukkot") {
+            artifactId = project.name
+            from(components["kotlin"])
+
+            artifact(sources)
+            artifact(javadocs)
+            artifact(final)
+
+            pom {
+                name = project.name
+                description = project.description
+                url = "https://github.com/zp4rker/bukkot"
+
+                packaging = "jar"
+
+                licenses {
+                    license {
+                        name = "The Apache License, Version 2.0"
+                        url = "https://www.apache.org/licenses/LICENSE-2.0.txt"
+                    }
+                }
+
+                developers {
+                    developer {
+                        id = "zp4rker"
+                        name = "Zaeem Parker"
+                        email = "iam@zp4rker.com"
+                    }
+                }
+
+                scm {
+                    connection = "scm:git:git://github.com/zp4rker/bukkot.git"
+                    url = "https://github.com/zp4rker/bukkot"
+                }
+
+                repositories {
+                    maven {
+                        val stagingUrl = "https://oss.sonatype.org/service/local/staging/deploy/maven2"
+                        val snapshotsUrl = "https://oss.sonatype.org/content/repositories/snapshots"
+                        url = uri(if (project.version.toString().endsWith("-SNAPSHOT")) snapshotsUrl else stagingUrl)
+                        credentials {
+                            username = System.getenv("OSSRH_USERNAME")
+                            password = System.getenv("OSSRH_PASSWORD")
+                        }
+                    }
+                }
+            }
         }
+    }
+}
+
+signing {
+    useGpgCmd()
+    sign(publishing.publications["bukkot"])
+}
+
+tasks.runServer {
+    minecraftVersion(mcVersion)
+    subprojects.forEach {
+        pluginJars(it.tasks.jar.map { jar -> jar.archiveFile })
     }
 }
